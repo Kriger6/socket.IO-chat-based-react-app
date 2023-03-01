467d40fd95a097fa7698af2410fbd740bb1b2e04
Kriger6 <sasa.palinkas6@gmail.com>
2023-03-01 12:33:10 +0100

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const App = () => {
+
+  return (
+    <div>My react app!</div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules : [
+            {
+                test: /\.tsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.js', '.ts', 'jsx']
+    },
+    plugins: [
+        new htmlWebpackPlugin(
+            {
+                template: './src/index.html'
+            }
+        )
+    ]
+}
